8b01962709103fc2b9cd82dab29a01cd1d48ac6a
Phina Putri Amalia <80098905+phinaputri@users.noreply.github.com>
2022-01-30 16:12:43 +0700

    Add files via upload

---
+++ Tugas 2 Data Type and Bitwise Operator/Nomor1.java
@@ -0,0 +1,14 @@
+/*  Nama : Phina Putri Aamalia
+    Kelas : Reg TI20A
+    NIM : 20200040063
+*/
+
+public class Nomor1 {
+    public static void main(String[] args){
+       int uang = 50000;
+    int hargaMainan = 55000;
+
+    String hasil = hargaMainan > uang ? "Tidak membeli" : "Membeli";
+    System.out.println(hasil);
+  }
+}
