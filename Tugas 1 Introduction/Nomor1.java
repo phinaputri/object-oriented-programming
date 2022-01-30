38b8664f58d41cbf88d842eacf54139c470bb4bf
Phina Putri Amalia <80098905+phinaputri@users.noreply.github.com>
2022-01-30 15:42:11 +0700

    Add files via upload

---
+++ Tugas 1 Introduction/Nomor1.java
@@ -0,0 +1,13 @@
+/*  Nama : Phina Putri Aamalia
+    Kelas : Reg TI20A
+    NIM : 20200040063
+*/
+
+public class Nomor1 {
+     public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("Dan Sangat Menyenangkan");
+    }
+    
+}
---
+++ Tugas 1 Introduction/Nomor2.java
@@ -0,0 +1,13 @@
+/*  Nama : Phina Putri Aamalia
+    Kelas : Reg TI20A
+    NIM : 20200040063
+*/
+
+import javax.swing.JOptionPane;
+
+public class Nomor2 {
+    public static void main (String[]args){
+        String namaTeman = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null, "Belajar "+namaTeman+" Sangat mudah   ");
+    }
+}
